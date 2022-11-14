b3631954de852befa4b5caf4a7b1687fed1009e1
rafa <homer512x@gmail.com>
2022-11-14 17:02:25 +0100

    archivos

---
+++ main.js
@@ -0,0 +1,10 @@
+function parImpar(num1) {
+	if (num1 % 2 == 0) {
+		console.log("El número analizado es par")
+	} else {
+		console.log("El número analizado es impar")
+	}
+}
+
+parImpar(2)
+parImpar(5)
